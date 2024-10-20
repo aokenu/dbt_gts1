8a1af19ecb5da1d82bbd6e81c8fe348a389540b5
aokenu <34852856+aokenu@users.noreply.github.com>
2024-10-20 11:45:02 +0100

    added space to audit_helper file

--- analyses/audit_helper_compare_relations.sql
+++ analyses/audit_helper_compare_relations.sql
@@ -1,7 +1,9 @@
 -- use this set if you are comparing to another dbt model
+
 {% set old_etl_relation=ref('stg_lineitem') %}
 
 -- this is your newly built dbt model 
+
 {% set dbt_relation=ref('fct_orders') %}
 
 {{ audit_helper.compare_relations(
